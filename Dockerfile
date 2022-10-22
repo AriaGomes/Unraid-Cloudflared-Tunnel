d144942edffe829601f24f1ca4631434d3a09946
AriaGomes <aria.gomes00@gmail.com>
2022-10-22 17:16:34 -0400

    dynamically get cloudflared release

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM alpine:latest
 WORKDIR /home
 RUN apk add --no-cache curl
-RUN curl -L --output cloudflared https://github.com/cloudflare/cloudflared/releases/download/2022.10.2/cloudflared-linux-amd64
-RUN apk add libc6-compat
+RUN apk add --no-cache libc6-compat
 COPY cloudflared.sh ./
 RUN chmod +x ./cloudflared.sh
-RUN chmod +x ./cloudflared
 ENTRYPOINT ./cloudflared.sh
--- cloudflared.sh
+++ cloudflared.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 token=$(printenv token)
-
+url=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest | grep -w "browser_download_url.*cloudflared-linux-amd64" | head -1 | cut -d : -f 2,3 | tr -d \")
+echo "Downloading latest cloudflared release..."
+curl -L --output "cloudflared" $url
+chmod +x ./cloudflared
 ./cloudflared tunnel run --token $token

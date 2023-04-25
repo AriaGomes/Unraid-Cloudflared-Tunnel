5e1968bb55a0bc011e6d666084600bf23f081e0a
AriaGomes <aria.gomes00@gmail.com>
2023-04-25 10:03:11 +0000

    put echo first

--- cloudflared.sh
+++ cloudflared.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+echo "Downloading latest cloudflared release..."
 token=$(printenv token)
 url=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest | jq -r '.assets[] | select(.browser_download_url | endswith("cloudflared-linux-amd64")) | .browser_download_url')
-echo "Downloading latest cloudflared release..."
 curl -L --output "cloudflared" $url
 chmod +x ./cloudflared
 ./cloudflared tunnel run --token $token

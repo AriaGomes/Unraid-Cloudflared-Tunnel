63d789966ae1dcce975ff3a635a46258178bda36
Aria Gomes <Aria.Gomes00@gmail.com>
2023-06-16 12:01:53 -0400

    Update cloudflared.sh

--- cloudflared.sh
+++ cloudflared.sh
@@ -4,4 +4,4 @@ token=$(printenv token)
 url=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest | jq -r '.assets[] | select(.browser_download_url | endswith("cloudflared-linux-amd64")) | .browser_download_url')
 curl -L --output "cloudflared" $url
 chmod +x ./cloudflared
-./cloudflared tunnel run --token $token
+./cloudflared tunnel run --token $token --no-autoupdate

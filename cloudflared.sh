b8852e0349472c28a0b72a37f8305b4cf7597d1f
Aria Gomes <Aria.Gomes00@gmail.com>
2024-03-17 01:24:24 -0400

    6 time out issue (#7)
    * Update README.md
    
    * Update cloudflared.sh
    
    * Update README.md
    
    * Update README.md

--- cloudflared.sh
+++ cloudflared.sh
@@ -10,14 +10,19 @@ cleanup() {
 # Trap signals and call cleanup function
 trap cleanup SIGINT SIGTERM
 
-# Ensure token is set
-if [ -z "$token" ]; then
-    echo "Error: 'token' environment variable is not set."
-    exit 1
+# Check if TUNNEL_TOKEN is set, if not, use token
+if [ -z "$TUNNEL_TOKEN" ]; then
+    if [ -z "$token" ]; then
+        echo "Error: 'TUNNEL_TOKEN' environment variable is not set."
+        exit 1
+    else
+        echo "Warning: 'TUNNEL_TOKEN' should be used instead of 'token'."
+        TUNNEL_TOKEN="$token"
+    fi
 fi
 
-# Start cloudflared
-cloudflared tunnel --no-autoupdate run --token $token &
+# Start cloudflared with TUNNEL_TOKEN or fallback to token
+cloudflared tunnel run --token $TUNNEL_TOKEN &
 pid=$!
 
 # Function to wait for cloudflared process to terminate
